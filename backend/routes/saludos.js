3f2b9d73a69700b846430a59f5bd194f87782520
Javier <jmartin@grupoloyal.com>
2023-11-21 14:03:55 +0100

    request

--- backend/routes/saludos.js
+++ backend/routes/saludos.js
@@ -2,24 +2,30 @@ const express = require('express');
 const router = express.Router();
 
 router.route('/')
-  .get((req, res) => {
-    res.end('GET: Hola mundo')
-  })
-  .post((req, res) => {
-    res.end('POST: Hola mundo')
-  })
-  .options((req, res) => {
-    res.end('OPTION: Hola mundo')
-  })
-router.route('/saluda/:id')
-  .get((req, res) => {
-    res.end(`GET ${req.params.id}: Hola mundo`)
-  })
-  .put((req, res) => {
-    res.end(`PUT ${req.params.id}: Hola mundo`)
-  })
-  .delete((req, res) => {
-    res.end(`DELETE ${req.params.id}: Hola mundo`)
-  })
+    .get((req, res) => {
+        
+         res.end(`GET: ${req.headers['accept-language']?.startsWith('es') ? 'Hola' : 'Hello'} ${req.query.nombre ?? 'mundo'}`)
+    })
+    .post((req, res) => {
+        res.end('POST: Hola mundo')
+    })
+    .options((req, res) => {
+        res.end('OPTION: Hola mundo')
+    })
+router.use('/user', require('./users'))
+router.route('/:id')
+    .get((req, res) => {
+        res.end(`GET ${req.params.id}: Hola mundo`)
+    })
+    .put((req, res) => {
+        res.end(`PUT ${req.params.id}: Hola mundo`)
+    })
+    .delete((req, res) => {
+        res.end(`DELETE ${req.params.id}: Hola mundo`)
+    })
+router.route('/:id/despide')
+    .get((req, res) => {
+        res.end(`GET ${req.params.id}: se despide`)
+    })
 
 module.exports = router;
