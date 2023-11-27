5b0e7859d3f40fb89e16fb5b66fcfe7afec63c09
Javier <jmartin@grupoloyal.com>
2023-11-27 14:00:02 +0100

    servicios

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { LoggerService } from '@my/core';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +12,11 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'AIRBUS';
+
+  constructor(out: LoggerService) {
+    out.error(`Es un error`)
+    out.warn(`Es un warn`)
+    out.info(`Es un info`)
+    out.log(`Es un log`)
+  }
 }
--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -2,7 +2,11 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { LoggerService } from '@my/core';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    LoggerService,
+    provideRouter(routes)
+  ]
 };
